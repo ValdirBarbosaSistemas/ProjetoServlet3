6afc03b668f4e2db59e83c11eb3840107971c432
Valdir <valdir.barbosasistemas@gmail.com>
2020-12-11 09:48:38 -0300

    Modificações no projeto
    Alterações realizadas no projeto servlet3

--- src/main/java/br/com/acme/agenda/utils/JPAUtil.java
+++ src/main/java/br/com/acme/agenda/utils/JPAUtil.java
@@ -3,13 +3,9 @@ package br.com.acme.agenda.utils;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import br.com.acme.agenda.model.Usuario;
-
 public class JPAUtil {
 
 	private static EntityManagerFactory factory;
-	
-	public static Usuario usuario;
 	
 	public static EntityManagerFactory getEntityManagerFactory() {
 		if(factory == null) {
